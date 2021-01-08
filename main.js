2d15685cc4493047fed3d1bcf4d61df1c05a0550
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-08 17:12:07 +0000

    Configuration for sessions implementation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const serveStatic = require('serve-static');
+const session = require('express-session');
+const sessionStore = require('connect-sqlite3')(session);
 const postsRouter = require('./router/posts.js');
 const categoriesRouter = require('./router/categories.js');
 const usersRouter = require('./router/users.js');
@@ -9,7 +11,25 @@ const app = new express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // INDEX METHODS' ROUTES
-app.use('/', serveStatic('./public', { dotfiles: 'ignore', extensions: ['html', 'htm'] }));
+app.use('/', serveStatic('./public', { 
+    dotfiles: 'ignore',
+    extensions: ['html', 'htm'] }));
+
+// API ROUTE SESSION CONFIGURATION
+app.use('/api', session({ 
+    secret: 'dwelltime!toBereckondWith', 
+    store: new sessionStore({ db: 'sessions.db', dir: './database' }), 
+    resave: true, 
+    saveUninitialized: false, 
+    cookie: { 
+        path: '/', 
+        httpOnly: true, 
+        secure: false, 
+        maxAge: null, 
+        sameSite: 'strict', 
+        name: 'api'
+    }
+}));
 
 // POSTS METHODS' ROUTES
 app.use('/api/posts', postsRouter);
