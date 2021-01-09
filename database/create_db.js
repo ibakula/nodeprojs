ef226aeb0056597cafed53c1d5c604ac02195499
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-09 11:14:19 +0000

    DB creation script add changes in structure

--- database/create_db.js
+++ database/create_db.js
@@ -5,5 +5,5 @@ const db = new sqlite.Database("./news.db", errorHandler.handleDbOpen);
 if (db != null) {
     db.run("CREATE TABLE posts (id INTEGER PRIMARY KEY, title TEXT NOT NULL, text TEXT NOT NULL, category_id INTEGER NOT NULL, author_id INTEGER NOT NULL, date TEXT NOT NULL);");
     db.run("CREATE TABLE categories (id INTEGER PRIMARY KEY, title TEXT NOT NULL);");
-    db.run("CREATE TABLE users (id INTEGER PRIMATY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, signup_date TEXT NOT NULL, login_date TEXT DEFAULT 0);");
+    db.run("CREATE TABLE users (id INTEGER PRIMATY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL UNIQUE, permissions INTEGER DEFAULT 0, signup_date TEXT NOT NULL, login_date TEXT DEFAULT 0);");
 }
