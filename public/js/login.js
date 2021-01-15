a221bf3e74db97c9c9120f44fe8bcadbb1453794
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-15 12:52:18 +0000

    Visitor pages improvement, check local storage instead

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,8 @@
-function handleStatus(response) {
-  if (response.data && 'userId' in response.data) {
-    let loginForm = document.getElementById("login_form");
-    for (let i = 0; i < loginForm.children.length; ++i) {
-      loginForm.children[i].remove();
-    }
-    loginForm.innerHTML = "<div id=\"login_output\" role=\"alert\" class=\"alert alert-danger p-4 mt-3\" bis_skin_checked=\"1\"><p>You are already logged in!</p></div>";
-    setTimeout(() => { window.location.href = "/index.html"; }, 4000);
+if (localStorage.getItem("userId") !== null) {
+  let loginForm = document.getElementById("login_form");
+  for (let i = 0; i < loginForm.children.length; ++i) {
+    loginForm.children[i].remove();
   }
+  loginForm.innerHTML = "<div id=\"login_output\" role=\"alert\" class=\"alert alert-danger p-4 mt-3\" bis_skin_checked=\"1\"><p>You are already logged in!<br>You are thus being redirected..</p></div>";
+  setTimeout(() => { window.location.href = "/index.html"; }, 4000);
 }
-
-function handleResponse(err) {
-  if (err.response) {
-          
-  }
-  else if (err.request) {
-          
-  }
-  else {
-          
-  }
-}
-
-axios.get('/api/user/status')
-  .catch(handleResponse)
-  .then(handleStatus)
-  .catch(handleResponse);
--- public/js/register.js
+++ public/js/register.js
@@ -1,27 +1,10 @@
-function handleStatus(response) {
-  if (response.data && 'userId' in response.data) {
-    let registerForm = document.getElementById("register_form");
-    for (let i = 0; i < registerForm.children.length; ++i) {
-      registerForm.children[i].remove();
-    }
-    registerForm.innerHTML = "<div id=\"login_output\" role=\"alert\" class=\"alert alert-danger p-4 mt-3\" bis_skin_checked=\"1\"><p>You are already logged in!</p></div>";
-    setTimeout(() => { window.location.href = "/index.html"; }, 4000);
+
+if (localStorage.getItem("userId") !== null) {
+  let registerForm = document.getElementById("register_form");
+  for (let i = 0; i < registerForm.children.length; ++i) {
+    registerForm.children[i].remove();
   }
+  registerForm.innerHTML = "<div id=\"login_output\" role=\"alert\" class=\"alert alert-danger p-4 mt-3\" bis_skin_checked=\"1\"><p>You are currently in an active session!<br><br>Thus you are being redirected..</p></div>";
+  setTimeout(() => { window.location.href = "/index.html"; }, 4000);
 }
 
-function handleResponse(err) {
-  if (err.response) {
-          
-  }
-  else if (err.request) {
-          
-  }
-  else {
-          
-  }
-}
-
-axios.get('/api/user/status')
-  .catch(handleResponse)
-  .then(handleStatus)
-  .catch(handleResponse);
