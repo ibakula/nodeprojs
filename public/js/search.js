fa0df75750b2d05aa2015fbf9a58c46a4d9e07f8
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-17 22:58:46 +0000

    [client-side]Search script completion

--- public/js/search.js
+++ public/js/search.js
@@ -1,16 +1,18 @@
 const navBarList = document.querySelector("header#top nav ul");
 const searchForm = navBarList.lastElementChild.firstElementChild;
-const inputField = searchForm.lastElementChild;
+const searchBtn = searchForm.lastElementChild;
 let info = false;
 let resultsFound = false;
 let finishedSearching = false;
 
 const siteContainer = document.body.firstElementChild;
 
-inputField.addEventListener("click", handleSearchBtnClick);
+searchBtn.addEventListener("click", handleSearchBtnClick);
 
 function handleSearchBtnClick(e) {
   e.preventDefault();
+  resultsFound = false;
+  finishedSearching = false;
   if (!isLoaded) {
     if (!info) {
       alert("Your search request will be executed after the initial requests are done executing.");
@@ -29,65 +31,90 @@ function handleSearchBtnClick(e) {
   searchElement.className = "container-fluid col-sm mb-5 p-3";
   searchElement.lastElementChild.appendChild(document.createElement("p"));
   searchElement.lastElementChild.lastElementChild.className = "lead";
-  searchElement.lastElementChild.lastElementChild.innerHTML = `Search results based on your inquiry, <a href="${location.pathname}">reload page<a> to go back!:`;
-  searchElement.appendChild(document.createElement("div"));
-  let dummyLoadingBarHtml = '<div class="mb-4 ml-2" style="background-color:#eeeeee;width:280px;height:3.5ex;"></div><div class="mb-3 ml-2" style="background-color:#eeeeee;width:480px;height:3.5ex;"></div><div class="mb-4 ml-2" style="background-color:#eeeeee;width:480px;height:3.5ex;"></div><div class="ml-2" style="background-color:#eeeeee;width:180px;height:3.5ex;"></div>';
+  searchElement.lastElementChild.lastElementChild.innerHTML = `Search results based on your inquiry, please <a href="${location.pathname}">reload page<a> to go back!:`;
+  let dummyLoadingBarHtml = '<div class="mb-4 ml-2" style="background-color:#eeeeee;width:280px;height:3.5ex;"></div><div class="mb-3 ml-2" style="background-color:#eeeeee;width:480px;height:3.5ex;"></div><div class="mb-4 ml-2" style="background-color:#eeeeee;width:480px;height:3.5ex;"></div><div class="mb-4 ml-2" style="background-color:#eeeeee;width:180px;height:3.5ex;"></div>';
   searchElement.innerHTML = searchElement.innerHTML + dummyLoadingBarHtml;
   siteContainer.insertBefore(searchElement, siteContainer.children[1]);
   // Articles
-  axios.get('/api/posts/last')
-  .catch(handleGetError)
+  let params = new URLSearchParams();
+  params.append("term", searchForm.firstElementChild.value);
+  axios.post('/api/posts/search', params)
+  .catch(handleError)
   .then(response => { 
-    performSearch(response, searchElement, 'posts'); 
+    handleLoadResults(response, 'posts', searchElement); 
   })
-  .catch(handleGetError);
+  .catch(handleError);
   // Users
-  axios.get('/api/users/last')
-  .catch(handleGetError)
+  axios.post('/api/users/search', params)
+  .catch(handleError)
   .then(response => {
-    performSearch(response, searchElement, 'users', true);
+    handleLoadResults(response, 'users', searchElement, true);
   })
-  .catch(handleGetError);
+  .catch(handleError);
   checkResults(searchElement);
   info = false;
 }
 
-function handleGetError(error) {
-}
-
-// Note: set third arg to true on the last table
-// on search sequence statement inside click function
-function performSearch(response, searchElement, type, last = false) {
-  if (!response.data || !('id' in response.data)) {
-    return;
-  }
-  for (let i = response.data.id; 
-    i < (response.data.id+1) && i >= 0; 
-    --i) {
-    axios.get(('/api/'+type+'/'+i))
-    .catch(handleGetError)
-    .then(res => { handleLoadResults(res, type, searchElement, last, (i == 1)); })
-    .catch(handleGetError);
-  }
+function handleError(error) {
 }
 
 // isLast boolean value which means that this is 
 // the VERY last search result we are waiting on
-// lastId is i == 0 in for loop of performSearch func
-function handleLoadResults(response, type, searchElement, isLast, lastId) {
-  if (!response.data || !('id' in response.data)) {
+function handleLoadResults(response, type, searchElement, isLast) {
+  if (!response.data || !Array.isArray(response.data) || response.data.length < 1) {
     // basically covers the same thing
     // as the last if statement at the end
     // of this function
-    if (isLast && lastId) {
+    if (isLast) {
        finishedSearching = true;
     }
     return;
   }
+  for (let i = 0; i < response.data.length; ++i) {
+    const divElement = document.createElement("div");
+    if (type == 'users') {
+      divElement.appendChild(document.createElement("a"));
+      divElement.lastElementChild.setAttribute("href", ("user.html?id="+response.data[i].id));
+      divElement.lastElementChild.appendChild(document.createElement("h4"));
+      divElement.lastElementChild.lastElementChild.className = "ml-2";
+      divElement.lastElementChild.lastElementChild.innerText = response.data[i].first_name + " " +  response.data[i].last_name;
+      divElement.appendChild(document.createElement("p"));
+      divElement.lastElementChild.className = "lead ml-2";
+      divElement.lastElementChild.innerText = "User profile";
+    }
+    else if (type == 'posts') {
+      divElement.appendChild(document.createElement("a"));
+      divElement.lastElementChild.setAttribute("href", ("article.html?id="+response.data[i].id));
+      divElement.lastElementChild.appendChild(document.createElement("h4"));
+      divElement.lastElementChild.lastElementChild.className = "ml-2";
+      divElement.lastElementChild.lastElementChild.innerText = response.data[i].title;
+      divElement.appendChild(document.createElement("p"));
+      divElement.lastElementChild.innerText = response.data[i].text;
+      divElement.lastElementChild.className = "lead ml-2";
+      divElement.appendChild(document.createElement("p"));
+      divElement.lastElementChild.className = "lead ml-2";
+      divElement.lastElementChild.innerText = "An article";
+    }
+    if (!resultsFound) {
+      hideDummies(searchElement);
+      resultsFound = true;
+    }
+    searchElement.appendChild(divElement);
+  }
   
   // the search has officially ended
-  if (isLast && response.data.id == 1) {
+  if (isLast) {
     finishedSearching = true;
+  }
+}
+
+function hideDummies(searchElement) {
+  const searchLength = searchElement.children.length;
+  for (let i = 1; i < searchLength; ++i) {
+    if (searchElement.children[1].firstElementChild != null) {
+      break;
+    }
+    searchElement.children[1].remove();
   }
 }
 
@@ -96,11 +123,9 @@ function checkResults(searchElementSection) {
     setTimeout(() => { checkResults(searchElementSection); }, 5000);
     return;
   }
+  const searchLength = searchElementSection.children.length;
+  hideDummies(searchElementSection);
   if (!resultsFound && searchElementSection != null) {
-    const searchLength = searchElementSection.children.length;
-    for (let i = 1; i < searchLength; ++i) {
-      searchElementSection.children[1].remove();
-    }
     searchElementSection.appendChild(document.createElement("div"));
     searchElementSection.className = "container-fluid col-sm mb-5 p-3";
     searchElementSection.lastElementChild.appendChild(document.createElement("p"));
