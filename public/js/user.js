91f7860a70e65c81c78b2cb27ad4701fac6ce201
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-21 19:18:25 +0000

    user landing page changes

--- public/js/user.js
+++ public/js/user.js
@@ -1,14 +1,12 @@
 let isLoaded = false;
 let userElementHeader = document.getElementById("top").nextElementSibling;
-let userContent =  document.getElementById("content").firstElementChild;
+let userContent = document.getElementById("content").firstElementChild;
 let params = new URLSearchParams(window.location.search);
 let userData = {
-  firstName: '',
-  lastName: '',
+  first_name: '',
+  last_name: '',
   password: '',
-  email: '',
-  lastLogin: '',
-  signupDate: ''
+  email: ''
 }
 
 function main() {
@@ -42,33 +40,65 @@ function LoadUserOwnData() {
     userContent.children[0].remove();
   }
   
-  userData.firstName = localStorage.getItem("first_name");
-  userData.lastName = localStorage.getItem("last_name");
+  userData.first_name = localStorage.getItem("first_name");
+  userData.last_name = localStorage.getItem("last_name");
   userData.email = localStorage.getItem("email");
-  userData.lastLogin = localStorage.getItem("last_login");
-  userData.signupDate = localStorage.getItem("signup_date");
+  
+  axios.get(('/api/users/'+localStorage.getItem("id")))
+  .catch(handleGetError)
+  .then(fetchRestUserData)
+  .catch(handleGetError);
+}
 
+function fetchRestUserData(response) {
+  if (response.data && 'permissions' in response.data) {
+    userData['permissions'] = response.data.permissions;
+    userData['signup_date'] = response.data.signup_date;
+    userData['login_date'] = response.data.login_date;
+  }
+  LoadFormData();
+}
+
+function LoadFormData() {
   userContent.appendChild(document.createElement("form"));
   userContent.lastElementChild.className = "ml-4";
   userContent.lastElementChild.id = "user_form";
+  userContent.style = "border:0px;";
 
   for (let item in userData) {
-    if ((userData[item] == null && 
-        item != 'password') || 
+    if ((userData[item] == null &&
+        item != 'password') ||
         userData[item].length < 1) {
       continue;
     }
+    let formNameStr = item;
+    if (formNameStr.search("_") > -1) {
+      formNameStr = formNameStr.replace("_", " ");
+    }
+    formNameStr = formNameStr.replace(formNameStr.charAt(0), formNameStr.charAt(0).toUpperCase());
     let divGroup = document.createElement("div");
     divGroup.className = "form-group";
     divGroup.appendChild(document.createElement("label"));
     divGroup.lastElementChild.setAttribute("for", `user_${item}`);
     divGroup.lastElementChild.className = "lead";
-    divGroup.lastElementChild.innerText = item;
+    divGroup.lastElementChild.innerText = formNameStr;
     divGroup.appendChild(document.createElement("input"));
     divGroup.lastElementChild.setAttribute("type", "text");
     divGroup.lastElementChild.className = "form-control form-control-md";
     divGroup.lastElementChild.id = `user_${item}`;
-    divGroup.lastElementChild.value = (item == 'password' ? '' : userData[item]);
+    if (item == 'permissions' || item == 'last_login' || item == 'signup_date') {
+      divGroup.lastElementChild.disabled = true;
+    }
+    if (item == 'password') {
+      divGroup.lastElementChild.setAttribute("placeholder", "Enter new password");
+    }
+    if (item == 'signup_date' || item == 'login_date') {
+      let date = new Date(parseInt(userData[item]));
+      divGroup.lastElementChild.value = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
+    }
+    else {
+      divGroup.lastElementChild.value = (item == 'password' ? '' : userData[item]);
+    }
     userContent.lastElementChild.appendChild(divGroup);
   }
   userContent.lastElementChild.appendChild(document.createElement("button"));
