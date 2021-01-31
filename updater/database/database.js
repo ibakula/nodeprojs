11cd26d6488177e8b96f1a83de630750dbc469ff
Ivan Bakula <ivan.bakula@outlook.com>
2021-01-31 21:45:00 +0000

    Fix sql generation and make task a computed key word

--- updater/database/database.js
+++ updater/database/database.js
@@ -20,13 +20,12 @@ class Database {
   }
   
   executeDeferredQuery(task, sql) {
-    let dbErr = this.error;
     let promise = new Promise((resolve, reject) => {
-      if (dbErr != null) {
+      if (this.error != null) {
         reject(dbErr);
       }
       else {
-        task(sql, (err, rows) => {
+        this.db[task](sql, (err, rows) => {
           if (err != null) {
             reject(err);
           }
@@ -52,17 +51,17 @@ class Database {
     else {
       sql += '* ';
     }
-    sql += `FROM ${table} `;
+    sql += `FROM ${table}`;
     if (matches != null && matches != undefined) {
-      sql += "WHERE ";
+      sql += " WHERE ";
       for (const item in matches) {
-        sql += `${item} = '${matches.item}', `;
+        sql += `${item} = `+ "'" + matches[item] + "', ";
       }
       sql = sql.slice(0, sql.length-2);
-      sql += ";";
     }
-    
-    return this.executeDeferredQuery(this.db.all, sql);
+    sql += ";";
+    console.log(sql);
+    return this.executeDeferredQuery('all', sql);
   }
   
   // params: Object
@@ -74,30 +73,31 @@ class Database {
         continue;
       }
       sql += `${i}, `;
-      val += `${params.i}, `;
+      val += "'" + params[i] + "', ";
     }
     sql = sql.slice(0, sql.length-2);
+    sql += ") ";
     val = val.slice(0, val.length-2);
     sql += val + ');';
-    
-    return this.executeDeferredQuery(this.db.run, sql);
+
+    return this.executeDeferredQuery('run', sql);
   }
   
   // params: Object, matches: Object
   update(params, matches) {
     let sql = `UPDATE ${params.table} SET `;
     for (const i in params) {
-      sql += `${i} = '${params.i}', `;
+      sql += `${i} = ` + "'" + params[i] + "', ";
     }
     sql = sql.slice(0, sql.length-2);
     sql += " WHERE ";
     for (const i in matches) {
-      sql += `${i} = ${matches.i}, `;
+      sql += `${i} = ${matches.i} AND `;
     }
-    sql = sql.slice(0, sql.length-2);
+    sql = sql.slice(0, sql.length-5);
     sql += ';';
 
-    return this.executeDeferredQuery(this.db.run, sql);
+    return this.executeDeferredQuery('run', sql);
   }
   
   // params: Object
@@ -109,7 +109,7 @@ class Database {
     sql = sql.slice(0, sql.length-5);
     sql += ';';
     
-    return this.executeDeferredQuery(this.db.run, sql);
+    return this.executeDeferredQuery('run', sql);
   }
 }
 
