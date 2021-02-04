32843da301658d39acfcd87c6e7e08232d57d0dd
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-04 11:43:54 +0000

    Bbc model more work

--- updater/model/bbc.js
+++ updater/model/bbc.js
@@ -1,9 +1,8 @@
 const Model = require('./model.js');
 
 class BbcLatestSectionModel extends Model {
-  constructor(html, sectionName) {
-    super(html);
-    this.timeNames = [ 'd', 'h' ];
+  constructor(html, sectionName, url) {
+    super(html, url);
     this.monthNames = [ 'Jan', 
       'Feb',
       'Mar',
@@ -18,16 +17,23 @@ class BbcLatestSectionModel extends Model {
       'Dec'
     ];
     this.sectionName = sectionName;
+    // This member assumes value of the last "inserted" posts time
+    this.lastPostTime = null;
   }
   
   *newsAddressListTodayIterator() {
     let latest = this.dom.window.document.body.querySelector("#site-container div:nth-child(3) ol");
     let now = new Date(Date.now());
+    let date = now.getDate() + " " +  this.monthNames[now.getMonth()];
     for (let i = 0; i < latest.children.length; ++i) {
       if (latest.children[i].tagName == "LI") {
         let postIdStr = latest.children[i].firstElementChild.getElementsByTagName("a")[0].getAttribute("href");
         if (postIdStr != null && postIdStr.search(this.sectionName) > -1) {
-          yield postIdStr;
+          let time = latest.children[i].querySelector("time span:last-child").innerHTML;
+          if (time.length > 5 && time.search(date) == -1 && (this.lastPostTime != null && this.lastPostTime == time)) {
+            continue;
+          }
+          yield { postIdStr, time };
         }
       }
     }
@@ -36,7 +42,7 @@ class BbcLatestSectionModel extends Model {
   get linksFullArticle() {
     let links = [];
     for (let item of this.newsAddressListTodayIterator()) {
-      links.push(item);
+      links.push(item.postIdStr);
     }
     return links;
   }
