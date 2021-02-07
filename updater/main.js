d72f3139a3df46235d0060325ce8acd28984e535
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-07 20:53:39 +0000

    Save main file

--- updater/main.js
+++ updater/main.js
@@ -1,3 +1,14 @@
 const axios = require('axios');
+const Database = require('./database/database.js');
+const db = new Database('./database/user.db');
 
+let userData = {};
 
+db.select('*', null, 'user')
+.then(rows => {
+  userData = rows[0];
+})
+.catch(error => {
+  console.error("DB Selection error: Failed to fetch user data");
+  console.error(error.message);
+});
