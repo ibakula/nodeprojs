e829d38145cd88c7f3ccd90eb89f814ad5f9a6d9
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-07 20:46:11 +0000

    Added HTML updater function

--- updater/model/model.js
+++ updater/model/model.js
@@ -4,6 +4,11 @@ class Model {
   constructor(html, address) {
     this.dom = new jsdom.JSDOM(html, { url: address, contentType: "text/html" });
   }
+
+  updateDom(html) {
+    let address = this.dom.window.origin + this.dom.window.pathname;
+    this.dom = new jsdom.JSDOM(html, { url: address, contentType: "text/html" });
+  }
 }
 
 module.exports = Model;
