2a0ba79ab72ce6054b3d5dd8494dc3d42a2a188b
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-10 19:03:26 +0100

    More code on controller

--- updater/controller/controller.js
+++ updater/controller/controller.js
@@ -1,47 +1,46 @@
-const Database = require('./database/database.js');
-const db = new Database('./database/user.db');
+const Database = require('../database/database.js');
+const QueryString = require('querystring');
 
 class Controller {
-  constructor(path) {
-    this.db = new Database('./database/user.db');
+  constructor(databasePath) {
+    this.db = new Database(databasePath);
     this.userData = {};
     this.httpConf = { 
-      withCredentials: true; 
+      withCredentials: true
     };
   }
   
-  handleMethodError(error) {
-    console.error(`HTTP Request error has occured: ${error.message}`);
-  }
-  
-  getCookie(response) {
+  onUserLogin(response) {
     
   }
   
-  setCookie() {
-    if(!('cookie_id' in userData)) {
-      axios.get('/api')
-      .then(this.getCookie)
+  // loginData = { email, password }
+  setCookie(loginData) {
+    if (!('cookie_id' in this.userData)) {
+      axios.post('/api/user/login', QueryString.stringify(loginData))
+      .then(onUserLogin)
       .catch(error => {
-        
+        console.error("setCookie error: Could not perform user login!");
+        console.error(error.message);
       });
     }
     else {
       this.httpConf.headers = {
         'Set-Cookie': `api=${this.userData.cookie_id};`
       };
+      // get login status
     }
   }
   
   init() { 
-    return Promise.all(db.select('*', null, 'user')
+    return this.db.select('*', null, 'user')
     .then(rows => {
       this.userData = rows[0];
     })
     .catch(error => {
       console.error("DB Selection error: Failed to fetch user data");
       console.error(error.message);
-    }));
+    });
   }
 }
 
