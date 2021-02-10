4561a7b3fbb7585e2ee95603987e36a2b73e0606
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-10 21:23:07 +0100

    Main function functionality tests conduction

--- updater/main.js
+++ updater/main.js
@@ -1,5 +1,12 @@
 const axios = require('axios');
 const controller = new (require('./controller/controller.js'))('./database/user.db');
 
-controller.init();
+Promise.resolve(controller.init('./database/user.db'))
+.then(v => {
+  console.log("App initialized!");
+})
+.catch(error => {
+  console.error("App failed to start 2!");
+  error != null ? console.error(error.message) : false;
+});;
 
