dff9dc263ef1b946195856c3ad0e3b41b0c8fbd5
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-11 21:22:04 +0100

    Added user data struct

--- updater/main.js
+++ updater/main.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 const controller = new (require('./controller/controller.js'))('./database/user.db');
 
+const authorData = {
+  'email': 'test@test.com',
+  'password' : 'testing'
+};
+
 Promise.resolve(controller.init('./database/user.db'))
 .then(v => {
   console.log("App initialized!");
