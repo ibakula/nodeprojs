812a88a73568edbfbd8592cae27c6441bf0678a3
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-11 21:22:28 +0100

    More code on the controller

--- updater/controller/controller.js
+++ updater/controller/controller.js
@@ -10,29 +10,65 @@ class Controller {
     };
   }
   
-  onUserLogin(response) {
-    
+  onRefreshStatus(response, resolve, reject) {
+    // Populate userData
+    // save to DB
+  }
+  
+  onUserLogin(response, resolve, reject) {
+    if ('data' in response && 'result' in response.data) {
+      if (response.data.result.search('Success!') != -1) {
+        this.userData['cookie_id'] = response.headers['Set-Cookie'];
+        this.updateHeaderConf();
+        axios.get('/api/user/status', { headers: this.httpConf.headers })
+        .then(response => {
+          this.onRefreshStatus(response, resolve, reject);
+        });
+        .catch(error => {
+          console.error("User status could not be obtained.");
+          console.error(error.message);
+          reject(error);
+        });
+      }
+      else { // else 'Failed!' with a property 'reason'
+        console.error("Login failed!");
+        console.error(response.data.reason);
+        reject(error);
+      }
+    }
+    else {
+      reject(new Error('No proper response was received from the server.'));
+    }
+  }
+  
+  updateHeaderConf() {
+    this.httpConf.headers = {
+      'Set-Cookie': `${this.userData.cookie_id};`
+    };
   }
   
   // loginData = { email, password }
   setCookie(loginData) {
-    if (!('cookie_id' in this.userData)) {
-      axios.post('/api/user/login', QueryString.stringify(loginData))
-      .then(onUserLogin)
-      .catch(error => {
-        console.error("setCookie error: Could not perform user login!");
-        console.error(error.message);
-      });
-    }
-    else {
-      this.httpConf.headers = {
-        'Set-Cookie': `api=${this.userData.cookie_id};`
-      };
-      // get login status
-    }
+    return new Promise((resolve, reject) => {
+      if (!('cookie_id' in this.userData)) {
+        axios.post('/api/user/login', QueryString.stringify(loginData))
+        .then(response => { 
+          this.onUserLogin(response, resolve, reject);
+        })
+        .catch(error => {
+          console.error("setCookie error: Could not perform user login!");
+          console.error(error.message);
+          reject(error);
+        });
+      }
+      else {
+        this.updateHeaderConf();
+        // get login status
+      }
+    });
   }
   
-  init(dbFilePath) { 
+  init(dbFilePath) {
     return new Promise((resolve, reject) => {
       this.db.open(dbFilePath)
       .then(() => {
