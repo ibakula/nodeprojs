71767b3224d2298206e91bc398dac1f3e5d08ae2
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-13 23:46:04 +0100

    Main: just some more testing related stuff

--- updater/main.js
+++ updater/main.js
@@ -7,11 +7,22 @@ const authorData = {
 };
 
 Promise.resolve(controller.init('./database/user.db'))
-.then(v => {
+.then(() => {
   console.log("App initialized!");
 })
 .catch(error => {
-  console.error("App failed to start 2!");
+  console.error("App failed to start!");
   error != null ? console.error(error.message) : false;
-});;
-
+})
+.then(() => {
+  controller.setCookie(authorData)
+  .then(() => {
+    console.log("User cookie was successfully set!");
+    console.log(controller.userData);
+  })
+  .catch(error => {
+    console.error("Could not create/update cookie data!");
+    error != null ? console.error(error.message) : false;
+    console.log(controller);
+  });
+});
