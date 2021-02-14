e3ae38141b903d7d1b47b6ade03b0d6b7262971a
Ivan Bakula <ivan.bakula@outlook.com>
2021-02-14 17:03:24 +0100

    Move main program functions to a class

---
+++ updater/controller/program.js
@@ -0,0 +1,50 @@
+const controller = new (require('./controller.js'))();
+
+class Main {
+  static start(authorData) {
+    return new Promise((resolve, reject) => {
+      let init = Promise.resolve(controller.init('./database/user.db'))
+      .then(() => {
+        console.log("App initialized!");
+      })
+      .catch(err => {
+        console.error("App failed to start!");
+        error != null ? console.error(error.message) : false;
+      });
+      
+      let setCookie = Promise.resolve(init).then(() => {
+        controller.setCookie(authorData)
+        .then(() => {
+          console.log("User cookie was successfully set!");
+          resolve();
+        })
+        .catch(error => {
+          console.error("Could not create/update cookie data!");
+          error != null ? console.error(error.message) : false;
+          if (error.message.search("expired") != -1) {
+            console.log("Session cookie has expired and was reset by the server.");
+            console.log("Resetting session cookie and attempting to create a new one!");
+            Promise.resolve(controller.deleteFromDB())
+            .then(() => {
+              console.log("Session cookie was wiped.");
+              controller.setCookie(authorData)
+              .then(() => {
+                console.log("User cookie was successfully set!");
+                resolve();
+              });
+            })
+            .catch(() => {
+              console.error("Could not delete user data.");
+              reject(new Error("Cookie deletion has failed!"));
+            });
+          }
+          else {
+            reject(error);
+          }
+        });
+      });
+    });
+  }
+}
+
+module.exports = Main;
