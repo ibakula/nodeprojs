9e1e01a894244ecf79184379812365a71b34bd15
Ivan Bakula <ivan.bakula@outlook.com>
2021-07-13 12:20:19 +0200

    Database class: fixed runtime errors and reworked some things

--- database/database.js
+++ database/database.js
@@ -1,25 +1,25 @@
 const sqlite = require('sqlite3').verbose();
 const VError = require('verror');
 
-let db = null;
+let dbRef = null;
 
 function openDatabase(filePath) {
   return new Promise((resolve, reject) => {
-    db = new sqlite.Database(filePath, err => {
+    dbRef = new sqlite.Database(filePath, err => {
       if (err) {
         let err2 = new VError(err, "Database open request failed");
         reject(err2);
         return;
       }
       resolve({
-        close: db.close,
-        run: db.run,
-        get: db.get,
-        all: db.all,
-        exec: db.exec
+        close: dbRef.close.bind(dbRef),
+        run: dbRef.run.bind(dbRef),
+        get: dbRef.get.bind(dbRef),
+        all: dbRef.all.bind(dbRef),
+        exec: dbRef.exec.bind(dbRef)
       });
     });
-  };
+  });
 }
 
 class Database {
@@ -29,10 +29,10 @@ class Database {
   
   async performTask(taskName, sql, ...params) {
     try {
-      await this.db;
+      const resDb = await this.db;
       
-      if (!(taskName in this.db)) {
-        throw new Error("attempted to execute a nonexistent task");
+      if (!resDb.hasOwnProperty(taskName)) {
+        throw new Error(`attempted to execute a nonexistent task "${taskName}"`);
       }
       
       /* 
@@ -40,13 +40,13 @@ class Database {
         which would only take an sql statement plus a callback
         thus avoiding any issues with binding "params" in place of callback 
       */
-      let call = this.db[taskName];
-      call.bind(this.db);
-      arguments.forEach(arg => {
-        if (arg) {
-          call = call.bind(arg);
+      let call = resDb[taskName];
+      for (const arg in arguments) {
+        if (arg == 0) {
+          continue;
         }
-      });
+        call = call.bind(null, arguments[arg]);
+      }
       
       return new Promise((resolve, reject) => {
         call((err, rows) => {
@@ -54,12 +54,20 @@ class Database {
             reject(new VError(err, `${taskName} operation has failed to execute`));
             return;
           }
+          
+          if (!rows && this instanceof Object && 
+             (this.hasOwnProperty('lastID') || 
+              this.hasOwnProperty('changes'))) {
+            resolve(this);
+            return;
+          }
+          
           resolve(rows);
         });
       });
     }
     catch (err) {
-      throw new VError(err, "run operation has failed");
+      throw new VError(err, `${taskName} operation has failed`);
     }
   }
 };
